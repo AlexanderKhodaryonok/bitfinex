adfc70a44bf979ee6f67ad12817f222e92642d7e
AlexanderKhodaryonok <alexanderkhodaryonok@gmail.com>
2021-07-17 17:04:08 +0300

    refactored

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -4,7 +4,11 @@ import styles from "./styles.module.css";
 
 function About() {
   const data = useAppSelector((state) => getAboutData(state));
-  return <div className={styles.container}>{data}</div>;
+  return (
+    <div className={styles.container}>
+      <h1>{data}</h1>
+    </div>
+  );
 }
 
 export default About;
